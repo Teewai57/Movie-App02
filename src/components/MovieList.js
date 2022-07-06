8c2dd52f2acc08c719f1266478ae3fb30f4d86eb
Teewai57 <bobtoyoabasi@gmail.com>
2022-07-06 11:59:50 +0100

    added img to movielist

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -3,7 +3,13 @@ import React from "react";
 const MovieList = (props) => {
     return(
         <>
-         {props.movies.map((movie, index) =>)}
+         {props.movies.map((movie, index) => 
+         <div>
+            <img src={movie.Poster} alt="movie"></img>
+         </div>
+         )}
         </>
     )
 }
+
+export default MovieList
