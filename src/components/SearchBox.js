71db627d5681e150e9eb686f4dcd0606db446f86
Teewai57 <bobtoyoabasi@gmail.com>
2022-07-07 03:33:13 +0100

    added input

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -2,7 +2,9 @@ import React from "react";
 
 const SearchBox = () => {
     return(
-
+        <div className="col col-sm-4">
+            <input className="form-control" placeholder="Type to search"></input>
+        </div>
     )
 }
 
