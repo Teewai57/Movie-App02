df62a99ec4702290714c36407fbc75e4a99bb28e
Teewai57 <bobtoyoabasi@gmail.com>
2022-07-07 03:39:43 +0100

    passed value props in search comp.

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -3,7 +3,13 @@ import React from "react";
 const SearchBox = () => {
     return(
         <div className="col col-sm-4">
-            <input className="form-control" placeholder="Type to search"></input>
+            <input 
+                className="form-control" 
+                onChange={(event) => props.setSearchValue(event.target.value) } 
+                value={props.value} 
+                placeholder="Type to search">
+                    
+            </input>
         </div>
     )
 }
